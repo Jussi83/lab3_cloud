9291280ef3643f5621c30c88d7bde15a738065b1
Jussi83 <57898256+Jussi83@users.noreply.github.com>
2022-04-16 12:31:08 +0200

    heroku bug fixed

--- client.js
+++ client.js
@@ -1,11 +1,11 @@
 const axios = require('axios');
 
 const data = {
-  data: 'O Hi this a test Car River Deer Car Bear and'  // response = 2 1 3 4 1 0 0 0 0 0 
-  //data: 'Malmö wicked man go prosper ye o dog' // response = 1 2 2 0 1 1 1 0 0 0
+  //data: 'O Hi this a test Car River Deer Car Bear and'  // response = 2 1 3 4 1 0 0 0 0 0 
+  data: 'Malmö wicked man go prosper ye o dog' // response = 1 2 2 0 1 1 1 0 0 0
 };
 
-axios.post('https://gentle-reaches-48154.herokuapp.com/', data)
+axios.post('https://gentle-reaches-48154.herokuapp.com/getWordLengthFrequency', data)
   .then((res) => {
     console.log(data);
     console.log();
